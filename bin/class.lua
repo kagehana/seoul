43865975330620a3b11e0f3bbf3197de65361b25
하늘의 한계 <qeoseol@gmail.com>
2025-02-22 21:45:36 -0600

    Create class.lua

---
+++ bin/class.lua
@@ -0,0 +1,33 @@
+-- metadata for classes
+local meta = {}
+
+function meta:__call(...)
+    return (setmetatable({}, self)):__new(...)
+end
+
+function meta:__tostring()
+    return ('class: ' .. self.__name)
+end
+
+-- class system implementation
+local function class(name, ...)
+    local new   = setmetatable({}, meta)
+    local bases = {...}
+
+    for _, base in ipairs(bases) do
+        for k1, v1 in pairs(base) do
+            if not k1:find('^__') then
+                new[k1] = v1
+            end
+        end
+    end
+
+    new.__name  = name
+    new.__bases = bases
+    new.__index = new
+
+    return new
+end
+
+-- here you go
+return class
