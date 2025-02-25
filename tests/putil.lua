b953dcd2bbb85033da482d47d2d31cd4be1289ee
하늘의 한계 <qeoseol@gmail.com>
2025-02-25 06:26:34 -0600

    Update putil.lua

--- tests/putil.lua
+++ tests/putil.lua
@@ -1,49 +1,67 @@
-local seoul = loadstring(game:HttpGet('https://github.com/kagehana/seoul/blob/main/seoul.lua?raw=true'))()
-local s  = seoul()
-local w  = s:window()
-local f  = w:folder()
+-- initialize the library
+local lib   = loadstring(game:HttpGet('https://github.com/kagehana/seoul/blob/main/seoul.lua?raw=true'))()
+local seoul = lib()
 
-local char = client.Character
+-- create a window
+local window = seoul:window('Hub')
 
-f:slider('Walkspeed', 0, 500, function(v)
-    char.Humanoid.WalkSpeed = tonumber(v)
-end)
+-- add a folder
+local folder = window:folder('Folder')
 
-f:button('Jump', function()
-    char.Humanoid.Jump = true
-end)
-
-f:toggle('Spin', function(s)
-    if s then
-        local Spin = Instance.new('BodyAngularVelocity')
-
-        Spin.Name            = 'Spin'
-        Spin.Parent          = char.HumanoidRootPart
-        Spin.AngularVelocity = Vector3.new(0, 20, 0)
-        Spin.MaxTorque       = Vector3.new(0, math.huge, 0)
-    else
-        for _, v in ipairs(char:GetDescendants()) do
-            if v.Name == 'Spin' then
-                v:Destroy();
-            end
-        end
+-- add a button
+folder:button({
+    name = 'Button',  -- button's name
+    call = function() -- button's callback
+        print('Button clicked!')
     end
-end)
+})
 
-local plist = {}
-
-for _, v in ipairs(players:GetPlayers()) do
-    insert(plist, v.Name)
-end
-
-f:dropdown('Goto', plist, function(v)
-    for _, vv in ipairs(players:GetPlayers()) do
-        if vv.Name == v then
-            char.HumanoidRootPart.CFrame = vv.Character.HumanoidRootPart.CFrame; break
-        end
+-- add a toggle
+folder:toggle({
+    name = 'Toggle',       -- toggle's name
+    call = function(state) -- toggle's callback
+        print('Toggled:', state)
     end
-end)
+})
 
-f:query('Say', function(v)
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(v, 'All')
-end)
+-- add a divider
+folder:divider('More')
+
+-- add a slider
+folder:slider({
+    name = 'Slider',       -- slider's name
+    min  = 0,              -- slider's minimum value
+    max  = 250,            -- slider's maximum value
+    call = function(value) -- slider's callback
+        print('Slid to:', value)
+    end
+})
+
+-- add a dropdown
+folder:dropdown({
+    name     = 'Dropdown',        -- dropdown's name
+    elements = {'1', '2', '3'},   -- dropdown's options
+    call     = function(selected) -- dropdown's callback
+        print('Selected option:', selected)
+    end
+})
+
+-- add a textbox/query
+folder:query({
+    placeholder = 'Enter text...', -- query's placeholder text
+    call        = function(input)  -- query's callback
+        print('Received input:', input)
+    end
+})
+
+-- make the window visible
+window:ready()
+
+-- notify the user with a default lifespan of 4.5s
+seoul:notify('Interface loaded!')
+
+-- optionally, you can pass a duration for the notification
+seoul:notify({
+    message  = 'Interface loaded!', -- notification's message
+    duration = 3.5                  -- notification's lifespan
+})
