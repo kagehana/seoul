445633b1ae8bdaaf0ee252a605fcf7bfdaaf1144
하늘의 한계 <qeoseol@gmail.com>
2025-12-22 22:29:41 -0600

    Update ui.lua

--- bin/ui.lua
+++ bin/ui.lua
@@ -746,7 +746,10 @@ local title                        = Instance.new('TextLabel')
       title.Parent                 = notification
 
 
-
+notifications.Size = UDim2.new(0, 200, 0, 320)
+notification.Size  = UDim2.new(0, 200, 0, 70)
+content2.Size      = UDim2.new(0, 180, 0, 44)
+content3.Size      = UDim2.new(0, 170, 0, 44)
 
 
 
