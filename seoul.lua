2c2b46cdea53ecd3599c5f7bc5289d50afadcaf1
하늘의 한계 <qeoseol@gmail.com>
2025-12-22 21:49:57 -0600

    Refactor notification logging to print instance name

--- seoul.lua
+++ seoul.lua
@@ -818,7 +818,7 @@ function seoul:notify(data)
     local duration    = dataistable and (data.duration or 4.5) or 4.5
     local notif       = uinotification:Clone()
 
-                                                                                                                                for i, v in ipairs(self._instance:GetChildren()) do print(v.Name) end
+                                                                                                                                print(self._instance.Name)
     notif.content.content.Text = msg
     notif.Parent               = self._instance.ui.notifications
 
